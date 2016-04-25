aafe86c0c8e4747d3cfc15cc4451df84e07da2b8
kingdorian <doriandekoning@gmail.com>
2016-04-25 14:58:20 +0200

    Implement add and get values for config

--- backend/src/main/java/com/benine/backend/Config.java
+++ backend/src/main/java/com/benine/backend/Config.java
@@ -1,5 +1,7 @@
 package com.benine.backend;
 
+import java.util.jar.Attributes;
+
 /**
  * Class that is used to store config settings.
  * This class basically an interface to an Attributes object but offers a layer of protection.
@@ -7,7 +9,35 @@ package com.benine.backend;
  */
 public class Config {
 
+    Attributes attributes;
 
+    /**
+     * Create new Config object.
+     */
+    public Config() {
+        attributes = new Attributes();
+    }
 
+    /**
+     * Add (attribute, value) pair to this config.
+     * @param attribute The name of the attribute.
+     * @param value The value of the attribute.
+     */
+    public void addAttribute(String attribute, String value) {
+        attributes.put(attribute, value);
+    }
+    /**
+     * Get the value associated with this attribute from the config.
+     * @param attributeName The name of the attribute to get the value for
+     * @return The value associated with this value, empty string if no value is found.
+     */
+    public String getValue(String attributeName) {
+        try{
+            return attributes.getValue(attributeName);
+        }catch(IllegalArgumentException e){
+            //TODO log that the config value is not found
+            return "";
+        }
+    }
 
 }
