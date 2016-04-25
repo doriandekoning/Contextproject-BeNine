d143afb49e077903775936d83e4b37e8d3eabbf4
jochem725 <jochemlugtenburg@gmail.com>
2016-04-25 16:05:41 +0200

    Add logger filewriter testcases

--- frontend/test/test_logger.js
+++ frontend/test/test_logger.js
@@ -1,7 +1,19 @@
 var logger = require('../app/logger');
 var assert = require('chai').assert;
+var path = require('path');
+var fs = require('fs');
 
 suite("Logger", function() {
+      suite("resetLog", function () {
+            test('should clear the logfile.', function(done) {
+                  logger.resetLog();
+
+                  var logcontent = fs.readFileSync(path.join(__dirname + '/../' + logger.getLogname()));
+                  assert.equal(logcontent.toString(), '');
+                  done();
+            });
+      });      
+
 	// Test suite for log function.
 	suite("formatLog", function () {
             // Check if all loglevels are formatted correctly.
@@ -9,7 +21,7 @@ suite("Logger", function() {
                   test('should format ' + level + ' correctly', function(done) {
                         var time = new Date().toLocaleString();
                         var message = "Test";
-                        assert.equal("[" + time + "][" + level + "]>\t" + message, logger.formatLog(level, message));
+                        assert.equal("[" + time + "][" + level + "]>\t\t" + message, logger.formatLog(level, message));
                         done();
                   });
             });
@@ -23,6 +35,19 @@ suite("Logger", function() {
                         assert.doesNotThrow(logger.logMessage.bind(logger, level, message), Error);
                         done();
                   });
+            });
+            test('should write correctly to the logfile', function(done) {
+                  var level = "INFO";
+                  var message = "Test";
+                  var logformat = logger.formatLog(level, message);
+                  logger.logMessage(level, message);
+
+                  fs.readFile(logger.getLogname(), 'utf-8', function(err, data) {
+                        if (err) throw err;
+                        var lastLine = data.trim().split('\n').slice(-1)[0];
+                        assert.equal(logformat, lastLine);
+                        done();
+                  });      
             });
             
       test('should throw an exception on an invalid level', function(done) {
