11ea55428149ed241fde51dc775d666e57d2d20d
kingdorian <doriandekoning@gmail.com>
2016-04-25 16:14:52 +0200

    Add config file reading into memory
    Functionality added to read a config file into memory, after that we can parse it line by line.

--- backend/src/main/java/com/benine/backend/ConfigReader.java
+++ backend/src/main/java/com/benine/backend/ConfigReader.java
@@ -1,9 +1,26 @@
 package com.benine.backend;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Class used for reading configuration details from a .config file and creates a new config object.
  * @author Dorian
  */
 public class ConfigReader {
-
+    /**
+     * Reads a config file from a string and returns an new config object.
+     * @param location the location of the file
+     * @return A new config object containing the attribute, value pares from the config file.
+     */
+    public static Config readConfig(String location) throws IOException {
+       BufferedReader br = new BufferedReader(new FileReader(location));
+        String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        return null;
+    }
 }
--- backend/src/test/java/benine/backend/ConfigReaderTest.java
+++ backend/src/test/java/benine/backend/ConfigReaderTest.java
@@ -1,17 +1,32 @@
 package benine.backend;
 
+import com.benine.backend.Config;
+import com.benine.backend.ConfigReader;
+
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.io.File;
+
+import static org.junit.Assert.fail;
+
 /**
- * Unit test class used to test the default configreader class.
+ * Unit test class used to test the default configReader class.
  * @author Dorian
  */
 public class ConfigReaderTest {
 
     @Test
     public final void ConfigReaderTest() {
-        Assert.assertTrue(false);
+        Config cfg = new Config();
+        cfg.addAttribute("Test", "1234");
+        try {
+            Config readConfig = ConfigReader.readConfig("resources"+ File.separator + "configs" + File.separator + "testconfig1.conf");
+            Assert.assertEquals(cfg, readConfig);
+        }catch(Exception e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 
 }
