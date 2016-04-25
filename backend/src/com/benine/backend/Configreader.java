e1bc459511bc3b92712b23b0f22715e500408194
kingdorian <doriandekoning@gmail.com>
2016-04-25 13:47:46 +0200

    Add moved classes to new backend package and create configreader

---
+++ backend/src/com/benine/backend/Configreader.java
@@ -0,0 +1,9 @@
+package com.benine;
+
+/**
+ * Class used for reading configuration details from a .config file and creates a new config object.
+ * @author Dorian
+ */
+public class ConfigReader {
+
+}
---
+++ backend/src/com/benine/backend/test/ConfigReaderTest.java
@@ -0,0 +1,18 @@
+package com.benine.test;
+
+import com.benine.ConfigReader;
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Unit test class used to test the default configreader class.
+ * @author Dorian
+ */
+public class ConfigReaderTest extends {
+
+    @Test
+    public final void testReadEmtpyFile() {
+        fail("Not implemented yet");
+    }
+
+}
