bd0e66d5a4ffb3b170a1b2d0725c36ecbcf5bffe
Bryan van Wijk <b.vanwijk@student.tudelft.nl>
2016-04-28 14:58:52 +0200

    Implement moveTo method in IPcamera.

--- cameramodule/src/main/java/com/benine/ipcameracontrol/Ipcamera.java
+++ cameramodule/src/main/java/com/benine/ipcameracontrol/Ipcamera.java
@@ -2,6 +2,13 @@ package com.benine.ipcameracontrol;
 
 import com.benine.Camera;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
 /**
  * Class to communicate with an IP Camera.
  * @author Bryan
@@ -20,8 +27,8 @@ public class Ipcamera implements Camera {
 
   @Override
   public void moveTo(double pan, double tilt, int panSpeed, int tiltSpeed) {
-    // TODO Auto-generated method stub
-    
+    sendCommand("%23APS" + convertPanToHex(pan) + convertTiltToHex(tilt) 
+                    + convertPanSpeedtoHex(panSpeed) + convertTiltSpeed(tiltSpeed));
   }
 
   @Override
@@ -110,6 +117,68 @@ public class Ipcamera implements Camera {
   public String getStreamLink() {
     // TODO Auto-generated method stub
     return null;
+  }
+  
+  private String sendCommand(String cmd) {
+    String res = null;
+    try {
+      InputStream com = new URL("http://" + ipadres + "/cgi-bin/aw_ptz?cmd=" + cmd + "&res=1").openStream();
+      BufferedReader buf = new BufferedReader(new InputStreamReader(com));
+      res = buf.readLine();
+      com.close();
+    } catch (IOException excep) {
+      excep.printStackTrace();
+    }
+    
+    return res;
+  }
+  
+  /**
+   * Convert the pan speed to hexadecimal form.
+   * And ensure it is in the range the camera supports.
+   * @param pan input pan speed.
+   * @return pan speed in hexadecimal form.
+   */
+  private String convertPanSpeedtoHex(int pan) {
+    pan = Math.min(29, pan);
+    pan = Math.max(0, pan);
+    return Integer.toHexString(pan);
+  }
+ 
+  /**
+   * Convert the tilt position in a range the camera supports.
+   * @param tilt input position in degrees.
+   * @return tilt position in hexadecimal form.
+   */
+  private String convertTiltToHex(double tilt) {
+    tilt = Math.min(210, tilt);
+    tilt = Math.max(-30, tilt);
+    tilt = ((tilt + 30) * 121.3541667) + 7284;
+    return Integer.toHexString((int) (tilt + 0.5));
+  }
+  
+  /**
+   * Convert the tilt speed to be only 0, 1 or 2.
+   * @param tilt input speed.
+   * @return supported speed by the camera.
+   */
+  private int convertTiltSpeed(int tilt) {
+    tilt = Math.min(2, tilt);
+    tilt = Math.max(0, tilt);
+    return tilt;
+  }
+  
+  /**
+   * Convert the pan position in a range the camera supports.
+   * @param pan input position in degrees.
+   * @return pan position in hexadecimal form.
+   */
+  private String convertPanToHex(double pan) {
+    pan = Math.min(175, pan);
+    pan = Math.max(-175, pan);
+    pan = ((pan + 175) * 121.3628571) + 11530;
+
+    return Integer.toHexString((int) (pan + 0.5));
   }
 
 }
