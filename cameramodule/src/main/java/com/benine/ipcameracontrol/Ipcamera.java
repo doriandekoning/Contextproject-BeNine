cc56b679457ba8aff6765d4473b9accd80f66c3e
Bryan van Wijk <b.vanwijk@student.tudelft.nl>
2016-04-28 14:24:31 +0200

    Add test for camera move command.

---
+++ cameramodule/src/main/java/com/benine/ipcameracontrol/Ipcamera.java
@@ -0,0 +1,117 @@
+package com.benine.ipcameracontrol;
+
+import com.benine.Camera;
+
+/**
+ * Class to communicate with an IP Camera.
+ * @author Bryan
+ */
+
+public class Ipcamera implements Camera {
+
+  String ipadres;
+
+  /**
+   *Create a new IP Camera object.
+   */
+  public Ipcamera(String ip) {
+    ipadres = ip;
+  }
+
+  @Override
+  public void moveTo(double pan, double tilt, int panSpeed, int tiltSpeed) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void move(int pan, int tilt) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public double[] getPosition() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int getZoomPosition() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int zoomTo(int zPos) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int zoom(int dir) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int getFocusPos() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public void setFocusPos(int pos) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void moveFocus(int speed) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void setAutoFocusOn(boolean on) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isAutoFocusOn() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void setAutoIrisOn(boolean on) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isAutoIrisOn() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void setIrisPos(int pos) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int getIrisPos() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public String getStreamLink() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
--- cameramodule/src/test/java/com/benine/ipcameracontrol/IPCameraTest.java
+++ cameramodule/src/test/java/com/benine/ipcameracontrol/IPCameraTest.java
@@ -1,41 +1,44 @@
 package com.benine.ipcameracontrol;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockserver.client.server.MockServerClient;
 import org.mockserver.junit.MockServerRule;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
+import org.mockserver.model.Parameter;
 import org.mockserver.verify.VerificationTimes;
 
 /**
- * Test class to test the IP Camera class
- * 
+ * Test class to test the IP Camera class.
+ * The mock server is used to simulate the camera.
  * @author Bryan
  */
 public class IPCameraTest {
 	
-	@Rule
-	public MockServerRule mockServerRule = new MockServerRule(this, 9000);
-	
-	private MockServerClient mockServerClient;
-	
-	@Before
-	public void setUp() throws Exception {
-		mockServerClient.when(HttpRequest.request("/test"))
-		.respond(HttpResponse.response().withBody("true"));
-	}
+  @Rule
+  public MockServerRule mockServerRule = new MockServerRule(this, 9000);
 
-	@Test
-	public void test() throws MalformedURLException, IOException {
-		new URL("http://127.0.0.1:9000/test").openStream();
-		mockServerClient.verify(HttpRequest.request("/test"),
-				VerificationTimes.once());
-	}
+  private MockServerClient mockServerClient;
+  private Ipcamera camera = new Ipcamera("127.0.0.1:9000");
+
+  private ArrayList<Parameter> parameterList;
+
+  @Test
+  public final void testMoveToHomePosition() {
+    parameterList = new ArrayList<Parameter>();
+    parameterList.add(new Parameter("res", "1"));
+    parameterList.add(new Parameter("cmd", "#APS80008000111"));
+
+    final HttpRequest request = HttpRequest.request("/cgi-bin/aw_ptz")
+                                    .withQueryStringParameters(parameterList);
+    mockServerClient.when(request).respond(HttpResponse.response().withBody("aPS80008000111"));
+    
+    //move with pan speed 17 and tilt speed 1
+    camera.moveTo(0, 180, 17, 1);
+    mockServerClient.verify(request, VerificationTimes.once());
+  }
 
 }
