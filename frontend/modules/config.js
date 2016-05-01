071b7ffdbe0eef69056019146a3e79b159b77b88
jochem725 <jochemlugtenburg@gmail.com>
2016-05-01 13:34:48 +0200

    Add basic config file handler for NodeJS server

---
+++ frontend/modules/config.js
@@ -0,0 +1,39 @@
+var fs = require("fs");
+var path = require("path");
+
+var default_config = {};
+
+default_config.server = "localhost";
+
+var self = {
+    getDefaultConfig: function () {
+        return default_config;
+    },
+    exists: function (config_path) {
+        try {
+            fs.statSync(config_path);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    },
+    load: function (config_path) {
+        // First check if the file exists, if it doesn't insert the default config.
+        if (self.exists(config_path)) {
+            try {
+                return JSON.parse(fs.readFileSync(config_path));
+            } catch (e) {
+                throw new Error("Config file at " + config_path + " could not be read.");
+            }
+        } else {
+            try {
+                fs.writeFileSync(config_path, JSON.stringify(default_config));
+                return default_config;
+            } catch (e) {
+                throw new Error("Could not write config file file at " + config_path);
+            }
+        }
+    }
+}
+
+module.exports = self;
