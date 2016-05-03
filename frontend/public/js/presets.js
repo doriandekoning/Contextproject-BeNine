421706fa465275b2892540d29b29e7a2f4a1ac19
jochem725 <jochemlugtenburg@gmail.com>
2016-05-03 23:01:00 +0200

    Add basic automatic preset row generation

--- frontend/public/js/presets.js
+++ frontend/public/js/presets.js
@@ -8,10 +8,31 @@ $.get('http://localhost:3000/api/getserver', function(data) {
     $.holdReady(false);
 });
 
+function addPresetRow() {
+    var presetrow = $('<div class="row"></div>');
+    var rowcontainer = $('<div class="col-xs-6"></div>');
+
+    for (var i = 0; i < 4; i++) {
+        rowcontainer.append($('<div class="col-xs-3"></div>'));
+    }
+
+    rowcontainer.children().each( function(index, elem) {
+        index++;
+        $(elem).append($('<div class = "available"><img src="public/test2.jpg"><h5>Preset ' + index + '</h5></div>'));
+    });
+
+    presetrow.append(rowcontainer);
+    $("#preset_area").append(presetrow);
+}
+
+
 // Document is ready, we can now manipulate it.
 $(document).ready(function() {
 
-    $("#preset_area").remove();
+    for (var i = 0; i < 4; i++) {
+        addPresetRow();
+    }
+
     console.log('Page has loaded successfully.');
 
 });
