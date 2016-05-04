4e26055762ebac13f7de91fea5afeec797f192a3
kingdorian <doriandekoning@gmail.com>
2016-05-04 21:03:27 +0200

    Implement focusingHandler

--- backend/src/main/java/com/benine/backend/http/FocussingHandler.java
+++ backend/src/main/java/com/benine/backend/http/FocussingHandler.java
@@ -2,8 +2,7 @@ package com.benine.backend.http;
 
 import com.benine.backend.Main;
 import com.benine.backend.camera.Camera;
-import com.benine.backend.camera.IrisCamera;
-import com.benine.backend.camera.ZoomingCamera;
+import com.benine.backend.camera.FocussingCamera;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
@@ -12,7 +11,7 @@ import java.util.jar.Attributes;
 /**
  * Created by dorian on 4-5-16.
  */
-public class IrisHandler extends RequestHandler {
+public class FocussingHandler extends RequestHandler {
   public void handle(HttpExchange exchange) throws IOException {
     //TODO add logging stuff
     // Extract camera id from function and amount to zoom in
@@ -21,15 +20,15 @@ public class IrisHandler extends RequestHandler {
     try {
       parsedURI = parseURI(exchange.getRequestURI().getQuery());
       Camera cam = Main.getCameraController().getCameraById(Integer.parseInt(parsedURI.getValue("id")));
-      IrisCamera irisCam = (IrisCamera)cam;
-      String autoOn = parsedURI.getValue("autoIrisOn");
+      FocussingCamera irisCam = (FocussingCamera) cam;
+      String autoOn = parsedURI.getValue("autoFocusOn");
       String setPos = parsedURI.getValue("position");
       if(autoOn != null) {
         boolean autoOnBool = Boolean.parseBoolean(autoOn);
-        irisCam.setAutoIrisOn(autoOnBool);
+        irisCam.setAutoFocusOn(autoOnBool);
       }
       if(setPos != null) {
-        irisCam.setIrisPos(Integer.parseInt(setPos));
+        irisCam.setFocusPos(Integer.parseInt(setPos));
       }
       response = "{\"succes\":\"true\"}";
     } catch (Exception e) {
