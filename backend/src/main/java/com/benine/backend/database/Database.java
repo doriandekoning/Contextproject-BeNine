33406e8ee89f47251bbaecd0f28ac288692a2729
Ege de Bruin <Ege de Bruin>
2016-05-05 19:38:28 +0200

    Fix checkstyle issues

--- backend/src/main/java/com/benine/backend/database/Database.java
+++ backend/src/main/java/com/benine/backend/database/Database.java
@@ -5,88 +5,99 @@ import java.util.ArrayList;
 
 /**
  * Interface for communication with the database.
+ *
  * @author Ege
  */
 public interface Database {
 
-	/**
-	 * Checks if database is connected.
-	 * @return true if database is connected
-	 * @throws SQLException
-     */
-	boolean isConnected() throws SQLException;
+  /**
+   * Checks if database is connected.
+   *
+   * @return true if database is connected
+   * @throws SQLException No right connection found
+   */
+  boolean isConnected() throws SQLException;
 
-	/**
-	 * Add a preset to the database.
-	 * @param camera ID of the camera
-	 * @param cameraPresetNumber ID of the preset for the camera
-	 * @param preset The preset to be added
-	 */
-	void addPreset(int camera, int cameraPresetNumber, DatabasePreset preset);
-	
-	/**
-	 * Delete a preset from the database.
-	 * @param camera ID of the camera
-	 * @param cameraPresetNumber ID of the preset for the camera
-	 */
-	void deletePreset(int camera, int cameraPresetNumber);
-	
-	/**
-	 * Update a preset to the database.
-	 * @param camera ID of the camera
-	 * @param cameraPresetNumber ID of the preset for the camera
-	 * @param preset The preset to be updated
-	 */
-	void updatePreset(int camera, int cameraPresetNumber, DatabasePreset preset);
-	
-	/**
-	 * Returns a preset of the camera.
-	 * @param camera ID of the camera
-	 * @param cameraPresetNumber ID of the preset of the camera
-	 * @return A preset
-	 */
-	DatabasePreset getPreset(int camera, int cameraPresetNumber);
-	
-	/**
-	 * Returns all the presets.
-	 * @return all the presets
-	 */
-	ArrayList<DatabasePreset> getAllPresets();
-	
-	/**
-	 * Returns all the presets of the camera.
-	 * @param cameraID ID of the camera
-	 * @return the presets of the given camera
-	 */
-	ArrayList<DatabasePreset> getAllPresetsCamera(int cameraID);
-	
-	/**
-	 * Tries to connect to database server.
-	 * @return True if the connection is succeeded.
-	 */
-	boolean connectToDatabaseServer();
+  /**
+   * Add a preset to the database.
+   *
+   * @param camera             ID of the camera
+   * @param cameraPresetNumber ID of the preset for the camera
+   * @param preset             The preset to be added
+   */
+  void addPreset(int camera, int cameraPresetNumber, DatabasePreset preset);
 
-	/**
-	 * Check if the database is present in the server.
-	 * @return True if the database is present, false otherwise
-     */
-	boolean checkDatabase();
+  /**
+   * Delete a preset from the database.
+   *
+   * @param camera             ID of the camera
+   * @param cameraPresetNumber ID of the preset for the camera
+   */
+  void deletePreset(int camera, int cameraPresetNumber);
 
-	/**
-	 * Creates new database, overrides the old one if there is one.
-     */
-	void resetDatabase();
+  /**
+   * Update a preset to the database.
+   *
+   * @param camera             ID of the camera
+   * @param cameraPresetNumber ID of the preset for the camera
+   * @param preset             The preset to be updated
+   */
+  void updatePreset(int camera, int cameraPresetNumber, DatabasePreset preset);
 
-	/**
-	 * Closes the connection to the server.
-	 */
-	void closeConnection();
+  /**
+   * Returns a preset of the camera.
+   *
+   * @param camera             ID of the camera
+   * @param cameraPresetNumber ID of the preset of the camera
+   * @return A preset
+   */
+  DatabasePreset getPreset(int camera, int cameraPresetNumber);
 
-	/**
-	 * Adds a camera to the database.
-	 * @param id The ID of the camera
-	 * @param ip The IP of the camera
-     */
-	void addCamera(int id, int ip, String name);
+  /**
+   * Returns all the presets.
+   *
+   * @return all the presets
+   */
+  ArrayList<DatabasePreset> getAllPresets();
+
+  /**
+   * Returns all the presets of the camera.
+   *
+   * @param cameraId ID of the camera
+   * @return the presets of the given camera
+   */
+  ArrayList<DatabasePreset> getAllPresetsCamera(int cameraId);
+
+  /**
+   * Tries to connect to database server.
+   *
+   * @return True if the connection is succeeded.
+   */
+  boolean connectToDatabaseServer();
+
+  /**
+   * Check if the database is present in the server.
+   *
+   * @return True if the database is present, false otherwise
+   */
+  boolean checkDatabase();
+
+  /**
+   * Creates new database, overrides the old one if there is one.
+   */
+  void resetDatabase();
+
+  /**
+   * Closes the connection to the server.
+   */
+  void closeConnection();
+
+  /**
+   * Adds a camera to the database.
+   *
+   * @param id The ID of the camera
+   * @param ip The IP of the camera
+   */
+  void addCamera(int id, int ip, String name);
 
 }
--- backend/src/main/java/com/benine/backend/database/DatabasePreset.java
+++ backend/src/main/java/com/benine/backend/database/DatabasePreset.java
@@ -3,100 +3,101 @@ package com.benine.backend.database;
 import org.json.simple.JSONObject;
 
 /**
- * A moving preset to be able to add to the database
+ * A moving preset to be able to add to the database.
  * @author Ege
- *
  */
 public class DatabasePreset {
-	
-	private int pan;
-	private int tilt;
-	private int zoom;
-	private int focus;
-	private int iris;
-	private boolean autofocus;
 
-	/**
-	 * Constructs a preset
-	 * @param pan The pan of the preset
-	 * @param tilt The tilt of the preset
-	 * @param zoom The zoom of the preset
-	 * @param focus The focus of the prest
-	 * @param iris The iris of the preset
-	 * @param autofocus The autofocus of the preset
-	 */
-	public DatabasePreset(int pan, int tilt, int zoom, int focus, int iris, boolean autofocus) {
-		this.pan = pan;
-		this.tilt = tilt;
-		this.zoom = zoom;
-		this.focus = focus;
-		this.iris = iris;
-		this.autofocus = autofocus;
-	}
+  private int pan;
+  private int tilt;
+  private int zoom;
+  private int focus;
+  private int iris;
+  private boolean autofocus;
 
-	/**
-	 * Returns a JSON representation of this object.
-	 * @return JSON representation of this object.
-	 */
-	public String toJSON() {
-		JSONObject json = new JSONObject();
+  /**
+   * Constructs a preset.
+   *
+   * @param pan       The pan of the preset
+   * @param tilt      The tilt of the preset
+   * @param zoom      The zoom of the preset
+   * @param focus     The focus of the prest
+   * @param iris      The iris of the preset
+   * @param autofocus The autofocus of the preset
+   */
+  public DatabasePreset(int pan, int tilt, int zoom, int focus, int iris, boolean autofocus) {
+    this.pan = pan;
+    this.tilt = tilt;
+    this.zoom = zoom;
+    this.focus = focus;
+    this.iris = iris;
+    this.autofocus = autofocus;
+  }
 
-		json.put("pan", new Integer(pan));
-		json.put("tilt", new Integer(tilt));
-		json.put("zoom", new Integer(zoom));
-		json.put("focus", new Integer(focus));
-		json.put("iris", new Integer(iris));
-		json.put("autofocus", new Boolean(autofocus));
+  /**
+   * Returns a JSON representation of this object.
+   *
+   * @return JSON representation of this object.
+   */
+  public String toJSON() {
+    JSONObject json = new JSONObject();
 
-		return json.toString();
-	}
+    json.put("pan", new Integer(pan));
+    json.put("tilt", new Integer(tilt));
+    json.put("zoom", new Integer(zoom));
+    json.put("focus", new Integer(focus));
+    json.put("iris", new Integer(iris));
+    json.put("autofocus", new Boolean(autofocus));
 
-	public int getPan() {
-		return pan;
-	}
+    return json.toString();
+  }
 
-	public void setPan(int pan) {
-		this.pan = pan;
-	}
+  public int getPan() {
+    return pan;
+  }
 
-	public int getTilt() {
-		return tilt;
-	}
+  public void setPan(int pan) {
+    this.pan = pan;
+  }
 
-	public void setTilt(int tilt) {
-		this.tilt = tilt;
-	}
+  public int getTilt() {
+    return tilt;
+  }
 
-	public int getZoom() {
-		return zoom;
-	}
+  public void setTilt(int tilt) {
+    this.tilt = tilt;
+  }
 
-	public void setZoom(int zoom) {
-		this.zoom = zoom;
-	}
+  public int getZoom() {
+    return zoom;
+  }
 
-	public int getFocus() {
-		return focus;
-	}
+  public void setZoom(int zoom) {
+    this.zoom = zoom;
+  }
 
-	public void setFocus(int focus) {
-		this.focus = focus;
-	}
+  public int getFocus() {
+    return focus;
+  }
 
-	public int getIris() {
-		return iris;
-	}
+  public void setFocus(int focus) {
+    this.focus = focus;
+  }
 
-	public void setIris(int iris) {
-		this.iris = iris;
-	}
+  public int getIris() {
+    return iris;
+  }
 
-	public boolean isAutofocus() {
-		return autofocus;
-	}
+  public void setIris(int iris) {
+    this.iris = iris;
+  }
 
-	public void setAutofocus(boolean autofocus) {
-		this.autofocus = autofocus;
-	}
+  public boolean isAutofocus() {
+    return autofocus;
+  }
+
+  public void setAutofocus(boolean autofocus) {
+    this.autofocus = autofocus;
+  }
 
 }
