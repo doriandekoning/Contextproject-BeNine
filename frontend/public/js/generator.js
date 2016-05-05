d3009de82c8d4a36de869ea33e13a35174f69dfb
jochem725 <jochemlugtenburg@gmail.com>
2016-05-05 17:17:32 +0200

    Remove debug code

--- frontend/public/js/generator.js
+++ frontend/public/js/generator.js
@@ -6,12 +6,6 @@ var presetcounter = 0;
 // Hold the ready until all data about the backend server is fetched.
 $.holdReady(true);
 
-// TODO: Remove Debug code linking to my machine. 
-$.get('http://10.0.1.203:3000/api/getserver', function(data) {
-    address = 'http://' + data.address + ':' + data.port;
-    $.holdReady(false);
-});
-
 $.get('http://localhost:3000/api/getserver', function(data) {
     address = 'http://' + data.address + ':' + data.port;
     $.holdReady(false);
