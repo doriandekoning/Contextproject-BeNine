7e18f881eaa3ce83d9fa2cd0380963a4130e23a1
jochem725 <jochemlugtenburg@gmail.com>
2016-05-05 21:05:31 +0200

    Add basic version of joystick control

--- frontend/public/js/generator.js
+++ frontend/public/js/generator.js
@@ -6,7 +6,7 @@ var presetcounter = 0;
 // Hold the ready until all data about the backend server is fetched.
 $.holdReady(true);
 
-$.get('http://localhost:3000/api/getserver', function(data) {
+$.get('http://10.0.1.203:3000/api/getserver', function(data) {
     address = 'http://' + data.address + ':' + data.port;
     $.holdReady(false);
 });
