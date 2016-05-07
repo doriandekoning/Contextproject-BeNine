0e158527ee3177b2061e7012514c6b9b4ec193f3
jochem725 <jochemlugtenburg@gmail.com>
2016-05-07 13:35:34 +0200

    Add proxy routing to backend server

--- frontend/modules/api.js
+++ frontend/modules/api.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var config = require('./config');
+var request = require('request');
 var router = express.Router();
 
-// API call /api/getserver returns a JSON object with data about the backend server.
-router.get('/getserver', function (req, res) {
+// Address of the backend server.
+var address = "http://" + config.get("backend_server") + ":" + config.get("backend_port");
+
+/**
+ * First the NodeJS server's own API is defined.
+ */
+// API call /api/getbackend returns a JSON object with data about the backend server.
+router.get('/getbackend', function (req, res) {
     res.json(
         {
             address: config.get('backend_server'),
             port: config.get('backend_port')
         }
     )
+});
+
+/**
+ * All /backend/... calls are rerouted to the backend server.
+ */
+router.get('/backend/*', function (req, res) {
+    request(address + req.url).pipe(res);
 });
 
 module.exports = router;
