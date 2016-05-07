4e0b2f5caf435ca36405af851d6dc8de5043fadb
jochem725 <jochemlugtenburg@gmail.com>
2016-05-07 13:10:07 +0200

    Add comment explaining how to hold the document if api data is needed

--- frontend/public/js/generator.js
+++ frontend/public/js/generator.js
@@ -3,13 +3,13 @@ var cameracounter = 0;
 var blockcounter = 0;
 var presetcounter = 0;
 
-// Hold the ready until all data about the backend server is fetched.
-$.holdReady(true);
-
-$.get('http://localhost:3000/api/getserver', function(data) {
-    address = 'http://' + data.address + ':' + data.port;
-    $.holdReady(false);
-});
+// The document ready can be hold as follows, might additional data from the server be needed.
+// $.holdReady(true);
+//
+// $.get('http://localhost:3000/api/getserver', function(data) {
+//     address = 'http://' + data.address + ':' + data.port;
+//     $.holdReady(false);
+// });
 
 // Document is ready, we can now manipulate it.
 $(document).ready(function() {
