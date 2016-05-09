84c0ce711a3a73e55ef9a3171f698e276631b0a8
NaomidR <naomi-ah@hotmail.com>
2016-05-09 21:01:27 +0200

    Add class that will allow creation of preset by tagging

---
+++ backend/src/main/java/com/benine/backend/http/CreatingPresetsHandler.java
@@ -0,0 +1,26 @@
+package com.benine.backend.http;
+
+import com.benine.backend.camera.CameraController;
+import com.benine.backend.database.DatabasePreset;
+import com.sun.net.httpserver.HttpExchange;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.jar.Attributes;
+
+/**
+ * @author naomi.
+ */
+public class CreatingPresetsHandler extends RequestHandler {
+
+  /**
+     * Handles a request
+     * @param exchange the exchange containing data about the request.
+     * @throws IOException when an error occurs with responding to the request.
+  */
+  public void handle(HttpExchange exchange) throws IOException {
+	
+	}
+}
