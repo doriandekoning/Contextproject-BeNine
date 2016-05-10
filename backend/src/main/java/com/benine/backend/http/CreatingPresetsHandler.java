8d988dae08dc8fa4d6be5f6c7eebc923afc658ec
NaomidR <naomi-ah@hotmail.com>
2016-05-10 10:31:17 +0200

    Add values needed to create new preset.

--- backend/src/main/java/com/benine/backend/http/CreatingPresetsHandler.java
+++ backend/src/main/java/com/benine/backend/http/CreatingPresetsHandler.java
@@ -1,5 +1,6 @@
 package com.benine.backend.http;
 
+import com.benine.backend.camera.CameraConnectionException;
 import com.benine.backend.camera.CameraController;
 import com.benine.backend.database.DatabasePreset;
 import com.sun.net.httpserver.HttpExchange;
@@ -11,26 +12,43 @@ import java.util.ArrayList;
 import java.util.jar.Attributes;
 
 /**
- * @author naomi.
- * Class that allows creation of a preset by tagging a camera viewpoint location.
- */
+ * @author naomi
+ * Class allows creation of a preset by tagging a camera viewpoint location.
+**/
 public class CreatingPresetsHandler extends RequestHandler {
   
 
   /**
-   * Create a new handler.
+   * Create a new handler for creating new presets.
    * @param controller the controller to interact with.
    */
   public CreatingPresetsHandler(CameraController controller) {
     super(controller);
   }
   
-/**
+  /**
      * Handles a request
      * @param exchange the exchange containing data about the request.
      * @throws IOException when an error occurs with responding to the request.
   */
   public void handle(HttpExchange exchange) throws IOException {
+    Attributes parsedURI;
+    try {
+      parsedURI = parseURI(exchange.getRequestURI().getQuery());
+      //Get the values needed to create a preset. 
+      String id = parsedURI.getValue("id");
+      String pan = parsedURI.getValue("pan");
+      String tilt = parsedURI.getValue("tilt");
+      String zoom = parsedURI.getValue("zoom");
+      String focus = parsedURI.getValue("focus");
+      String iris = parsedURI.getValue("iris");
+      String autofocus = parsedURI.getValue("autoFocusOn");
+    }
+    
+    catch (MalformedURIException e) {
+      //TODO Log exception
+      System.out.println(e);
+    }
 
   }
 }
