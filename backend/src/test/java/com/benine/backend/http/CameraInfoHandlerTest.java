8cae919e9258c917fa7b22dc12c4962231c160f0
kingdorian <doriandekoning@gmail.com>
2016-05-11 10:47:17 +0200

    Fix getcamerainfohandler test

--- backend/src/test/java/com/benine/backend/http/CameraInfoHandlerTest.java
+++ backend/src/test/java/com/benine/backend/http/CameraInfoHandlerTest.java
@@ -1,8 +1,11 @@
 package com.benine.backend.http;
 
+import com.benine.backend.camera.Camera;
 import com.benine.backend.camera.CameraController;
 import com.benine.backend.camera.ZoomingCamera;
 import com.sun.net.httpserver.HttpExchange;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.Test;
 
 import java.io.OutputStream;
@@ -17,20 +20,30 @@ public class CameraInfoHandlerTest {
 
   @Test
   public void testGetInfo() throws Exception {
+    // Setup mocks
     HttpExchange exchange = mock(HttpExchange.class);
     OutputStream out = mock(OutputStream.class);
     ZoomingCamera cam = mock(ZoomingCamera.class);
     CameraController camController = new CameraController();
     camController.addCamera(cam);
+    // Setup request uri
     URI uri = new  URI("http://localhost/camera/");
     when(exchange.getRequestURI()).thenReturn(uri);
     when(exchange.getResponseBody()).thenReturn(out);
-    CameraInfoHandler cHandler = new CameraInfoHandler(camController);
+    CameraInfoHandler cHandler = new CameraInfoHandler(camController, -1);
     try {
-      zHandler.handle(exchange);
+      cHandler.handle(exchange);
     } catch (Exception e) {
       e.printStackTrace();
     }
-    verify(cam).toJSON(2);
+    // Create expected json object
+    JSONObject json = new JSONObject();
+    JSONArray array = new JSONArray();
+    for(Camera camera : camController.getCameras()) {
+      array.add(camera.toJSON());
+    }
+    json.put("cameras", array);
+    // Test if correct json string is written to the output
+    verify(out).write(json.toString().getBytes());
   }
 }
