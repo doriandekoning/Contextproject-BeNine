ed42938add239e02bb60d4e76fa40e808f8b6e69
NaomidR <naomi-ah@hotmail.com>
2016-05-11 18:44:07 +0200

    Change big test to smaller tests.

--- backend/src/test/java/com/benine/backend/http/PresetCreationHandlerTest.java
+++ backend/src/test/java/com/benine/backend/http/PresetCreationHandlerTest.java
@@ -1,11 +1,10 @@
 package com.benine.backend.http;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.OutputStream;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.benine.backend.camera.CameraConnectionException;
@@ -13,60 +12,54 @@ import com.benine.backend.camera.CameraController;
 import com.benine.backend.camera.Position;
 import com.benine.backend.camera.ipcameracontrol.IPCamera;
 import com.benine.backend.database.DatabasePreset;
-import com.sun.net.httpserver.HttpExchange;
-
-import org.junit.Assert;
-import org.junit.Before;
 
 public class PresetCreationHandlerTest {
-  private CameraController camera;
+
+  private CameraController controller;
   private PresetCreationHandler handler;
-  private HttpExchange exchangeMock;
-  private OutputStream out;
+  private IPCamera ipcamera;
+  private DatabasePreset preset;
   
   @Before
-  public void initialize(){
-    camera = mock(CameraController.class);
-    handler = new PresetCreationHandler(camera);
-    exchangeMock = mock(HttpExchange.class);
-    out = mock(OutputStream.class);
-    when(exchangeMock.getResponseBody()).thenReturn(out);
-  }
-  
-  @Test
-  public void testGetCameraPositions() throws CameraConnectionException {
-    CameraController controller = mock(CameraController.class);
-    PresetCreationHandler handler = new PresetCreationHandler(controller);
-    IPCamera ipcamera = mock(IPCamera.class);
+  public void setUp() throws CameraConnectionException{
+    controller = mock(CameraController.class);
+    handler = new PresetCreationHandler(controller);
+    ipcamera = mock(IPCamera.class);
     when(ipcamera.getFocusPos()).thenReturn(33);
     when(ipcamera.getIrisPos()).thenReturn(50);
     when(ipcamera.getPosition()).thenReturn(new Position(0, 0));
     when(ipcamera.getZoomPosition()).thenReturn(100);  
     when(ipcamera.isAutoFocusOn()).thenReturn(true);
-
-    DatabasePreset preset = new DatabasePreset(0,0,100,33,50,true);
+    preset = new DatabasePreset(0,0,100,33,50,true);
+    
+  }
+  @Test
+  public void testGetFocusPos() {
     Assert.assertEquals(preset.getFocus(), handler.createPreset(ipcamera).getFocus());
+  }
+  @Test
+  public void testGetIrisPos() {
     Assert.assertEquals(preset.getIris(), handler.createPreset(ipcamera).getIris());
+  } 
+  
+  @Test
+  public void testGetPanPos() {
     Assert.assertEquals(preset.getPan(), handler.createPreset(ipcamera).getPan());
+  } 
+  
+  @Test
+  public void testGetTiltPos() {
     Assert.assertEquals(preset.getTilt(), handler.createPreset(ipcamera).getTilt());
+  } 
+  
+  @Test
+  public void testGetZoomPos() {
     Assert.assertEquals(preset.getZoom(), handler.createPreset(ipcamera).getZoom());
-    Assert.assertEquals(preset.isAutofocus(), handler.createPreset(ipcamera).isAutofocus());
-  }
-
-
+  } 
   
   @Test
-  public void testResponseMessageTrue() throws Exception {
-    String response = "{\"succes\":\"true\"}"; 
-    handler.responseMessage(exchangeMock, true);
-    verify(exchangeMock).sendResponseHeaders(200, response.length());
-  }
-  
-  @Test
-  public void testResponseMessageFalse() throws Exception {
-    String response = "{\"succes\":\"false\"}"; 
-    handler.responseMessage(exchangeMock, false);
-    verify(exchangeMock).sendResponseHeaders(200, response.length());
+  public void testAutofocusOn() {
+    Assert.assertEquals(preset.isAutofocus(), handler.createPreset(ipcamera).isAutofocus());    
   }
 
 }
