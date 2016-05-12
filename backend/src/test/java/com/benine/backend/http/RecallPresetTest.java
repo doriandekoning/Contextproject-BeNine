89c358062c079415216ca6d1976a54de57c86faf
NaomidR <naomi-ah@hotmail.com>
2016-05-12 13:06:26 +0200

    Delete failing test.

--- backend/src/test/java/com/benine/backend/http/RecallPresetTest.java
+++ backend/src/test/java/com/benine/backend/http/RecallPresetTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 
 public class RecallPresetTest {
 
-  @Test
-  public void test() {
-    fail("Not yet implemented");
-  }
+//  @Test
+//  public void test() {
+//    fail("Not yet implemented");
+//  }
 
 }
