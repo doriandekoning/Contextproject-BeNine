358ae689c71ec14ae0f8740d3c052f3dce5911b3
jochem725 <jochemlugtenburg@gmail.com>
2016-05-17 15:53:09 +0200

    Add basic mjpeg header parsing

--- backend/src/main/java/com/benine/backend/video/MJPEGStreamReader.java
+++ backend/src/main/java/com/benine/backend/video/MJPEGStreamReader.java
@@ -1,13 +1,16 @@
 package com.benine.backend.video;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MJPEGStreamReader {
 
     private Stream stream;
-    private InputStream streamdata;
+    private BufferedInputStream streamdata;
 
     /**
      * Creates a new MJPEGStreamReader.
@@ -15,8 +18,48 @@ public class MJPEGStreamReader {
      */
     public MJPEGStreamReader(URL url) throws IOException{
         this.stream = new Stream(url);
-        this.streamdata = stream.getInputStream();
+        this.streamdata = new BufferedInputStream(stream.getInputStream());
+
+        try {
+            getImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private int checkNextByte() throws IOException {
+        int b = 0;
+
+        streamdata.mark(1);
+        b = streamdata.read();
+        streamdata.reset();
+
+        return b;
+    }
+
+    private Byte[] getImage() throws IOException {
+        // We store the header in here.
+        StringWriter header = new StringWriter(128);
+
+        while (checkNextByte() != 255) {
+            header.write(streamdata.read());
+        }
+
+        System.out.println(getContentLength(header.toString()));
+        System.out.println(header.toString());
+
+        return null;
+    }
+
+    private int getContentLength(String header) {
+        Pattern number = Pattern.compile("-?\\d+");
+        Matcher matcher = number.matcher(header);
+
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group());
+        } else {
+            return -1;
+        }
+    }
 
 }
