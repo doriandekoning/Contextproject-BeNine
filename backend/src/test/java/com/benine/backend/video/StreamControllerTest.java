9c0a4de14ade1fff369605449f6856c843fa96e9
jochem725 <jochemlugtenburg@gmail.com>
2016-05-18 22:47:05 +0200

    Fix test removal This is a fix for the previous commit which accidentially didn't have the tests removed.

--- backend/src/test/java/com/benine/backend/video/StreamControllerTest.java
+++ backend/src/test/java/com/benine/backend/video/StreamControllerTest.java
@@ -1,15 +1,11 @@
 package com.benine.backend.video;
 
 import com.benine.backend.camera.Camera;
-import com.benine.backend.camera.ipcameracontrol.IPCamera;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.HashMap;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 
 /**
  * Created by Jochem on 18-05-16.
@@ -27,27 +23,6 @@ public class StreamControllerTest {
 
     controller = new StreamController(streams, threads);
   }
-
-  @Test
-  public void testAddCameraIpThread() {
-    IPCamera cam = Mockito.mock(IPCamera.class);
-    Mockito.when(cam.getStreamLink()).thenReturn("http://localhost");
-
-    controller.addCamera(cam);
-
-    Mockito.verify(threads).put(eq(0), any(Thread.class));
-  }
-
-  @Test
-  public void testAddCameraIpStream() {
-    IPCamera cam = Mockito.mock(IPCamera.class);
-    Mockito.when(cam.getStreamLink()).thenReturn("http://localhost");
-
-    controller.addCamera(cam);
-
-    Mockito.verify(streams).put(eq(0), any(MJPEGStreamReader.class));
-  }
-
 
   @Test
   public void testAddCameraWithoutStreamThread() {
