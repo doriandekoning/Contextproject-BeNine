4e1cc060e4d151acce4f4eeaddaeda17986eda00
jochem725 <jochemlugtenburg@gmail.com>
2016-05-22 16:06:18 +0200

    Fix camerainfohandler no response bug

--- backend/src/main/java/com/benine/backend/http/CameraInfoHandler.java
+++ backend/src/main/java/com/benine/backend/http/CameraInfoHandler.java
@@ -2,6 +2,7 @@ package com.benine.backend.http;
 
 import com.benine.backend.ServerController;
 import com.benine.backend.camera.Camera;
+import com.benine.backend.camera.CameraController;
 import com.benine.backend.camera.FocussingCamera;
 import com.benine.backend.camera.IrisCamera;
 import com.benine.backend.camera.MovingCamera;
@@ -22,7 +23,11 @@ public class CameraInfoHandler extends CameraRequestHandler {
 
   private CameraMovingHandler moveHandler;
 
+  private CameraIrisHandler irisHandler;
+
   private CameraZoomHandler zoomHandler;
+
+  private CameraController cameraController;
 
   /**
    * Constructor for a new CameraInfoHandler, handling the /camera/ request.
@@ -30,8 +35,10 @@ public class CameraInfoHandler extends CameraRequestHandler {
   public CameraInfoHandler() {
     this.streamHandler = new CameraStreamHandler();
     this.focusHandler = new CameraFocusHandler();
+    this.irisHandler = new CameraIrisHandler();
     this.moveHandler = new CameraMovingHandler();
     this.zoomHandler = new CameraZoomHandler();
+    this.cameraController = ServerController.getInstance().getCameraController();
   }
 
   @Override
@@ -39,6 +46,8 @@ public class CameraInfoHandler extends CameraRequestHandler {
           throws IOException, ServletException {
 
     String cameraInfo = getCameraController().getCamerasJSON();
+
+    boolean routed = false;
 
     if (checkValidCameraID(request)) {
       String route = getRoute(request);
@@ -48,34 +57,37 @@ public class CameraInfoHandler extends CameraRequestHandler {
         case "mjpeg":
           if (cam.getStreamType() == StreamType.MJPEG) {
             streamHandler.handle(s, request, req, res);
+            routed = true;
           }
           break;
         case "focus":
           if (cam instanceof FocussingCamera) {
             focusHandler.handle(s, request, req, res);
+            routed = true;
           }
           break;
         case "move":
           if (cam instanceof MovingCamera) {
             moveHandler.handle(s, request, req, res);
+            routed = true;
           }
           break;
         case "iris":
           if (cam instanceof IrisCamera) {
-            focusHandler.handle(s, request, req, res);
+            irisHandler.handle(s, request, req, res);
+            routed = true;
           }
           break;
         case "zoom":
           if (cam instanceof ZoomingCamera) {
             zoomHandler.handle(s, request, req, res);
+            routed = true;
           }
           break;
-        default: {
-          respond(request, res, cameraInfo);
-          request.setHandled(true);
-        }
       }
-    } else {
+    }
+
+    if (!routed) {
       respond(request, res, cameraInfo);
       request.setHandled(true);
     }
@@ -89,10 +101,36 @@ public class CameraInfoHandler extends CameraRequestHandler {
   private boolean checkValidCameraID(Request request) {
     int id = getCameraId(request);
 
-    ServerController controller = ServerController.getInstance();
-    Camera camera = controller.getCameraController().getCameraById(id);
+    Camera camera = cameraController.getCameraById(id);
 
     return id > 0 && camera != null;
   }
 
+  public CameraStreamHandler getStreamHandler() {
+    return streamHandler;
+  }
+
+  public CameraFocusHandler getFocusHandler() {
+    return focusHandler;
+  }
+
+  public CameraMovingHandler getMoveHandler() {
+    return moveHandler;
+  }
+
+  public CameraZoomHandler getZoomHandler() {
+    return zoomHandler;
+  }
+
+  public void setHandlers(CameraStreamHandler streamHandler,  CameraFocusHandler focusHandler,
+                     CameraIrisHandler irishandler,
+                     CameraMovingHandler movingHandler,
+                     CameraZoomHandler zoomHandler) {
+
+    this.streamHandler = streamHandler;
+    this.focusHandler = focusHandler;
+    this.moveHandler = movingHandler;
+    this.zoomHandler = zoomHandler;
+    this.irisHandler = irishandler;
+  }
 }
