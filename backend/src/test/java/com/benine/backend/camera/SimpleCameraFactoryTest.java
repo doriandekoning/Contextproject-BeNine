2b933842e1cb39f1a60be02c6599a9d2eb96050b
jochem725 <jochemlugtenburg@gmail.com>
2016-05-23 22:28:03 +0200

    Remove test stream dependencies

--- backend/src/test/java/com/benine/backend/camera/SimpleCameraFactoryTest.java
+++ backend/src/test/java/com/benine/backend/camera/SimpleCameraFactoryTest.java
@@ -1,13 +1,12 @@
 package com.benine.backend.camera;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.File;
-
+import com.benine.backend.ServerController;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.benine.backend.ServerController;
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleCameraFactoryTest {
   
@@ -34,7 +33,7 @@ public class SimpleCameraFactoryTest {
     SimpleCameraFactory factory = new SimpleCameraFactory();
     Camera result = factory.createCamera(2);
     SimpleCamera camera = new SimpleCamera();
-    camera.setStreamLink("http://131.180.123.51/zm/cgi-bin/nph-zms?mode=jpeg&monitor=2&scale=100&buffer=100");
+    camera.setStreamLink("http://test");
     camera.setMacAddress("simplecamera3");
     assertEquals(camera, result);
   }
