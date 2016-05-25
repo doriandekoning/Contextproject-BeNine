c6c830d6640590183f00e14390130d9650fda61b
kingdorian <doriandekoning@gmail.com>
2016-05-25 20:48:19 +0200

    Fix checkstyle warnings

--- backend/src/main/java/com/benine/backend/http/AddTagHandler.java
+++ backend/src/main/java/com/benine/backend/http/AddTagHandler.java
@@ -3,11 +3,11 @@ package com.benine.backend.http;
 import com.benine.backend.ServerController;
 import org.eclipse.jetty.server.Request;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 /**
  * Created by BeNine on 25-5-16.
  */
@@ -17,7 +17,7 @@ public class AddTagHandler extends RequestHandler {
   public void handle(String s, Request request, HttpServletRequest httpServletRequest,
                      HttpServletResponse httpServletResponse) throws IOException, ServletException {
     String tagName = request.getParameter("name");
-    if(tagName != null) {
+    if (tagName != null) {
       ServerController.getInstance().getPresetController().addTag(tagName);
     }
     respondSuccess(request, httpServletResponse);
