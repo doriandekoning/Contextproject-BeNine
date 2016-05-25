79603ec7b5e4b0ab3adcbbd37d39a270c6fe0449
kingdorian <doriandekoning@gmail.com>
2016-05-25 21:45:04 +0200

    Add tests RemoveTagHandler

---
+++ backend/src/main/java/com/benine/backend/http/RemoveTagHandler.java
@@ -0,0 +1,19 @@
+package com.benine.backend.http;
+
+import org.eclipse.jetty.server.Request;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dorian on 25-5-16.
+ */
+public class RemoveTagHandler extends RequestHandler {
+  @Override
+  public void handle(String s, Request request, HttpServletRequest httpServletRequest,
+                     HttpServletResponse httpServletResponse) throws IOException, ServletException {
+
+  }
+}
