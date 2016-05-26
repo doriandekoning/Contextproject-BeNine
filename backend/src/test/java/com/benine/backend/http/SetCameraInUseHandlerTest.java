2671ba2e680ab775d998deb239697cdbefa174f4
kingdorian <doriandekoning@gmail.com>
2016-05-27 00:43:04 +0200

    Add tests SetCameraInUseHandler

--- backend/src/test/java/com/benine/backend/http/SetCameraInUseHandlerTest.java
+++ backend/src/test/java/com/benine/backend/http/SetCameraInUseHandlerTest.java
@@ -1,7 +1,75 @@
 package com.benine.backend.http;//TODO add Javadoc comment
 
+import com.benine.backend.camera.Camera;
+import org.eclipse.jetty.util.MultiMap;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
 /**
  *
  */
-public class SetCameraInUseHandlerTest {
+public class SetCameraInUseHandlerTest  extends CameraRequestHandlerTest {
+
+  Camera cam = mock(Camera.class);
+
+  @Before
+  public void initialize() throws IOException {
+    super.initialize();
+    when(cameracontroller.getCameraById(42)).thenReturn(cam);
+  }
+
+  @Test
+  public void testInuseTrue() throws IOException, ServletException {
+    setPath("/42/inuse?inuse=true");
+
+    MultiMap<String> parameters = new MultiMap<>();
+    parameters.add("inuse", "true");
+    setParameters(parameters);
+
+    getHandler().handle(target, requestMock, httprequestMock, httpresponseMock);
+
+    verify(cam).setInUse(true);
+    verify(requestMock).setHandled(true);
+  }
+
+  @Test
+  public void testInuseNoParam() throws IOException, ServletException {
+    setPath("/42/inuse");
+
+    MultiMap<String> parameters = new MultiMap<>();
+    setParameters(parameters);
+
+    getHandler().handle(target, requestMock, httprequestMock, httpresponseMock);
+
+    verify(cam, never()).setInUse(true);
+    verify(requestMock).setHandled(true);
+  }
+
+  @Test
+  public void testInuseAlreadyInuse() throws IOException, ServletException {
+    setPath("/42/inuse?inuse=true");
+
+    when(cam.isInUse()).thenReturn(true);
+
+    MultiMap<String> parameters = new MultiMap<>();
+    parameters.add("inuse", "true");
+    setParameters(parameters);
+
+    getHandler().handle(target, requestMock, httprequestMock, httpresponseMock);
+
+    verify(cam, never()).setInUse(true);
+    verify(requestMock).setHandled(true);
+    //cleanup
+    when(cam.isInUse()).thenReturn(false);
+  }
+
+  @Override
+  public CameraRequestHandler supplyHandler() {
+    return new SetCameraInUseHandler();
+  }
 }
