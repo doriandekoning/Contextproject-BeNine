cfc39197d0e203f90255b05b6c17d3dea9f7cca3
jochem725 <jochemlugtenburg@gmail.com>
2016-05-26 14:54:45 +0200

    Add cookie adding and deletion

--- frontend/public/js/settings.js
+++ frontend/public/js/settings.js
@@ -1,11 +1,23 @@
-var settings;
-
-console.log(document.cookie)
-
 function getSettings() {
+    var settings = Cookies.getJSON();
 
+    // Check if there are settings set, if not generate defaults.
+    if ($.isEmptyObject(settings)) {
+        setSetting("testvalue", 1);
+    }
+
+    return settings;
 }
 
 function setSetting(parameter, value) {
+    Cookies.set(parameter, value, { expires: 5000 });
+}
 
+function clearCookies() {
+    var settings = Cookies.getJSON();
+    var keys = Object.keys(settings);
+
+    for(var i = 0; i < keys.length; i++) {
+        Cookies.remove(keys[i]);
+    }
 }
