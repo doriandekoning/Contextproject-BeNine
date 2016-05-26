4bf119289aa0b1bfb822d63064d7531cf4b72885
Bryan van Wijk <b.vanwijk@student.tudelft.nl>
2016-05-26 16:46:05 +0200

    Add sliders to settings window

--- frontend/public/js/settings.js
+++ frontend/public/js/settings.js
@@ -3,7 +3,10 @@ function getSettings() {
 
     // Check if there are settings set, if not generate defaults.
     if ($.isEmptyObject(settings)) {
-        setSetting("testvalue", 1);
+		setSetting("zoom", 1);
+		setSetting("iris", 5);
+		setSetting("focus", 3);
+		setSetting("joystick", 5);
     }
 
     return settings;
@@ -21,3 +24,36 @@ function clearCookies() {
         Cookies.remove(keys[i]);
     }
 }
+
+//Below is the javascript for the settings modal
+function loadSettings() {
+	var settings = getSettings();
+	var zoom = settings.zoom;
+	var iris = settings.iris;
+	var focus = settings.focus;
+	var joystick = settings.joystick;
+	$('#zoomsense_lbl').text(zoom);
+	$('#zoomsense').val(zoom);
+	$('#irissense_lbl').text(iris);
+	$('#irissense').val(iris);
+	$('#focussense_lbl').text(focus);
+	$('#focussense').val(focus);
+	$('#joysticksense_lbl').text(joystick);
+	$('#joysticksense').val(joystick);
+}
+
+$('#zoomsense').on('input', function () {
+    $('#zoomsense_lbl').text($(this).val());
+});
+
+$('#irissense').on('input', function () {
+    $('#irissense_lbl').text($(this).val());
+});
+
+$('#focussense').on('input', function () {
+    $('#focussense_lbl').text($(this).val());
+});
+
+$('#joysticksense').on('input', function () {
+    $('#joysticksense_lbl').text($(this).val());
+});
