7d719d62fbb18e9b56c9da9c53d6756f545c2fed
kingdorian <doriandekoning@gmail.com>
2016-06-01 13:59:02 +0200

    Add AutoPresetCreator Interface

---
+++ backend/src/main/java/com/benine/backend/AutoPresetCreator.java
@@ -0,0 +1,19 @@
+package com.benine.backend;
+
+import com.benine.backend.camera.Camera;
+
+import java.util.Collection;
+
+/**
+ *
+ */
+public interface AutoPresetCreator {
+
+  /**
+   * Automatically creates presets for the selected camera.
+   * @param cam the camera to create presets for.
+   * @return A collection of the created presets.
+   */
+  Collection<Preset> createPresets(Camera cam);
+
+}
