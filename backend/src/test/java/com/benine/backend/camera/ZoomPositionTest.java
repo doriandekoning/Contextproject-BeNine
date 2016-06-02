cf3f7a17de5ab8fb5a68d0af5785148ae2572632
kingdorian <doriandekoning@gmail.com>
2016-06-02 23:30:30 +0200

    Add tests ZoomPositionEquals

--- backend/src/test/java/com/benine/backend/camera/ZoomPositionTest.java
+++ backend/src/test/java/com/benine/backend/camera/ZoomPositionTest.java
@@ -28,4 +28,38 @@ public class ZoomPositionTest {
     ZoomPosition expectedPos = new ZoomPosition(50, 20, 50);
     Assert.assertEquals(actualPos, expectedPos);
   }
+
+  @Test
+  public final void testEqualsEquals() {
+    ZoomPosition pos1 = new ZoomPosition(new Position(50, 20), 50);
+    ZoomPosition pos2 = new ZoomPosition(new Position(50, 20), 50);
+    Assert.assertEquals(pos1, pos2);
+  }
+
+  @Test
+  public final void testEqualsNotEqualZoom() {
+    ZoomPosition pos1 = new ZoomPosition(new Position(50, 20), 3);
+    ZoomPosition pos2 = new ZoomPosition(new Position(50, 20), 50);
+    Assert.assertNotEquals(pos1, pos2);
+  }
+
+  @Test
+  public final void testEqualsNotEqualPosition() {
+    ZoomPosition pos1 = new ZoomPosition(new Position(3, 20), 50);
+    ZoomPosition pos2 = new ZoomPosition(new Position(50, 20), 50);
+    Assert.assertNotEquals(pos1, pos2);
+  }
+
+  @Test
+  public final void testEqualsNotEqualNull() {
+    ZoomPosition pos1 = new ZoomPosition(new Position(3, 20), 50);
+    Assert.assertNotEquals(pos1, null);
+  }
+
+  @Test
+  public final void testEqualsNotEqualOtherObject() {
+    ZoomPosition pos1 = new ZoomPosition(new Position(3, 20), 50);
+    Position pos2 = new Position(50, 20);
+    Assert.assertNotEquals(pos1, pos2);
+  }
 }
