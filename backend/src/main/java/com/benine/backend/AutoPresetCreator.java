c35bfdbc62f373a2aa5aef058ba31f94da58ffc9
kingdorian <doriandekoning@gmail.com>
2016-06-02 10:37:13 +0200

    Move createPresets method to AutoPresetCreator

--- backend/src/main/java/com/benine/backend/AutoPresetCreator.java
+++ backend/src/main/java/com/benine/backend/AutoPresetCreator.java
@@ -2,21 +2,41 @@ package com.benine.backend;
 
 import com.benine.backend.camera.CameraConnectionException;
 import com.benine.backend.camera.MovingCamera;
+import com.benine.backend.camera.Position;
 import com.benine.backend.camera.ipcameracontrol.IPCamera;
+import com.benine.backend.camera.ipcameracontrol.ZoomPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.TimeoutException;
 
 /**
  *
  */
-public interface AutoPresetCreator {
+public abstract class AutoPresetCreator {
 
   /**
    * Automatically creates presets for the selected camera.
+   * Calll generatePositions to get the positions of the presets.
    * @param cam the camera to create presets for.
    * @return A collection of the created presets.
    */
-  Collection<Preset> createPresets(IPCamera cam) throws CameraConnectionException, InterruptedException, TimeoutException;
+  public Collection<Preset> createPresets(IPCamera cam)
+    throws CameraConnectionException, InterruptedException, TimeoutException {
+    Position camStartPos = cam.getPosition();
+    ArrayList<Preset> presets = new ArrayList<Preset>();
+    for (ZoomPosition pos : generatePositions(cam)) {
+      int zoom = cam.getZoomPosition();
+      cam.moveTo(pos, 30, 2);
+      //TODO change to waitUntillAtPosition(zoom)
+      cam.waitUntilAtPosition(pos, pos.getZoom(), 2000);
+        presets.add(new PresetFactory().createPreset(cam, 2, 30));
+    }
+    return presets;
+  }
+
+
+
+  public abstract Collection<ZoomPosition> generatePositions(IPCamera cam);
 
 }
