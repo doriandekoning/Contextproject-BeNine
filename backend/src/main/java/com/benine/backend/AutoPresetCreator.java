e3e1bbf9f05ef73f1378ee18bf94ba3dd8f96f72
NaomidR <naomi-ah@hotmail.com>
2016-06-02 12:42:56 +0200

    Fix checkstyle warnings

--- backend/src/main/java/com/benine/backend/AutoPresetCreator.java
+++ backend/src/main/java/com/benine/backend/AutoPresetCreator.java
@@ -1,17 +1,16 @@
 package com.benine.backend;
 
-import com.benine.backend.camera.CameraConnectionException;
-import com.benine.backend.camera.MovingCamera;
-import com.benine.backend.camera.Position;
-import com.benine.backend.camera.ipcameracontrol.IPCamera;
-import com.benine.backend.camera.ipcameracontrol.ZoomPosition;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.TimeoutException;
 
+import com.benine.backend.camera.CameraConnectionException;
+import com.benine.backend.camera.Position;
+import com.benine.backend.camera.ZoomPosition;
+import com.benine.backend.camera.ipcameracontrol.IPCamera;
+
 /**
- *
+ * Created by BeNine on 25-5-16.
  */
 public abstract class AutoPresetCreator {
 
@@ -20,6 +19,9 @@ public abstract class AutoPresetCreator {
    * Calll generatePositions to get the positions of the presets.
    * @param cam the camera to create presets for.
    * @return A collection of the created presets.
+   * @throws CameraConnectionException exception thrown when the camera connection failed.
+   * @throws InterruptedException exception thrown when camera is interrupted.
+   * @throws TimeoutException exception thrown when there's a timeout.
    */
   public Collection<Preset> createPresets(IPCamera cam)
     throws CameraConnectionException, InterruptedException, TimeoutException {
@@ -30,13 +32,19 @@ public abstract class AutoPresetCreator {
       cam.moveTo(pos, 30, 2);
       //TODO change to waitUntillAtPosition(zoom)
       cam.waitUntilAtPosition(pos, pos.getZoom(), 2000);
-        presets.add(new PresetFactory().createPreset(cam, 2, 30));
+      presets.add(new PresetFactory().createPreset(cam, 2, 30));
     }
     return presets;
   }
 
 
-
-  protected abstract Collection<ZoomPosition> generatePositions(IPCamera cam) throws CameraConnectionException;
+  /**
+   * 
+   * @param cam the camera for which a position is generated.
+   * @return zoompostion
+   * @throws CameraConnectionException exception thrown when the camera connection failed.
+   */
+  protected abstract Collection<ZoomPosition> generatePositions(IPCamera cam) 
+      throws CameraConnectionException;
 
 }
