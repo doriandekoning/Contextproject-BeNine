c20fb92a3e8b8ab33c9275aecc625f70643c6ad9
kingdorian <doriandekoning@gmail.com>
2016-06-02 10:56:06 +0200

    Add methods larger grid

--- backend/src/main/java/com/benine/backend/PresetPyramidCreator.java
+++ backend/src/main/java/com/benine/backend/PresetPyramidCreator.java
@@ -39,27 +39,27 @@ public class PresetPyramidCreator extends AutoPresetCreator {
     ArrayList<ZoomPosition> positions = new ArrayList<>();
     for (int level = 0; level < levels; level++ ) {
       int zoomlevel = (IPCamera.MAX_ZOOM - cam.getZoomPosition())* (level/levels);
-      Collection<Position> positionsInLayer =  generatePositionsLayer(zoomlevel);
+      Collection<Position> positionsInLayer =  generatePositionsLayer(new ZoomPosition(cam.getPosition(), zoomlevel));
       positionsInLayer.forEach( p -> positions.add(new ZoomPosition(p, zoomlevel)));
     }
     return positions;
   }
 
 
-  private Collection<Position> generatePositionsLayer(int zoomlevel) {
+  private Collection<Position> generatePositionsLayer(ZoomPosition curPos) {
     ArrayList<Position> positions = new ArrayList<>();
 
     // 1 is completely zoomed out, 0 completely zoomed in
 
-    final double zoomCoefficient =  1 - ((zoomlevel - IPCamera.MIN_ZOOM)/(IPCamera.MAX_ZOOM));
+    final double zoomCoefficient =  1 - ((curPos.getZoom() - IPCamera.MIN_ZOOM)/(IPCamera.MAX_ZOOM));
     final double curHorFov = (IPCamera.HORIZONTAL_FOV_MAX - IPCamera.HORIZONTAL_FOV_MIN) * zoomCoefficient;
     final double curVerFov = (IPCamera.VERTICAL_FOV_MAX - IPCamera.VERTICAL_FOV_MIN) * zoomCoefficient;
     double betweenVer = (curVerFov/columns) - (overlap*curVerFov);
     double betweenHor = (curHorFov/columns) - (overlap*curHorFov);
     // Calculate start positions
     // TODO Change to current cam locations as starting point
-    double startPan = 0 - ((columns-1)*(betweenHor/2));
-    double startTilt = 180 - ((rows-1)*(betweenVer/2));
+    double startPan = curPos.getPan() - ((columns-1)*(betweenHor/2));
+    double startTilt = curPos.getTilt() - ((rows-1)*(betweenVer/2));
 
     for (int row = 0; row < rows; row++) {
       for (int column = 0; column < columns; column++) {
--- backend/src/test/java/com/benine/backend/PresetPyramidCreatorTest.java
+++ backend/src/test/java/com/benine/backend/PresetPyramidCreatorTest.java
@@ -3,6 +3,7 @@ package com.benine.backend;
 import com.benine.backend.camera.CameraConnectionException;
 import com.benine.backend.camera.Position;
 import com.benine.backend.camera.ipcameracontrol.IPCamera;
+import com.benine.backend.camera.ipcameracontrol.ZoomPosition;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,62 +26,53 @@ public class PresetPyramidCreatorTest extends AutoPresetCreatorTest {
 
   IPCamera cam = mock(IPCamera.class);
 
-
   @Before
-  public final void setup() throws CameraConnectionException, TimeoutException, InterruptedException {
-   // doReturn().when(cam).waitUntilAtPosition(any(Position.class), anyInt(), anyLong());
-    doReturn(new Position(30, 20)).when(cam).getPosition();
-    doReturn(0).when(cam).getZoomPosition();
-    doAnswer(new Answer<Void>() {
-      @Override
-      public Void answer(InvocationOnMock invocation) {
-        return null;
-      }
-    }).when(cam).moveTo(any(Position.class), anyInt(), anyInt());
-    doAnswer(new Answer<Void>() {
-      @Override
-      public Void answer(InvocationOnMock invocation) {
-        return null;
-      }
-    }).when(cam).waitUntilAtPosition(any(Position.class), anyInt(), anyInt());
+  public void setup() throws CameraConnectionException {
+    when(cam.getPosition()).thenReturn(new Position(0, 180));
+    when(cam.getZoomPosition()).thenReturn(0);
   }
 
   @Test
-  public final void testCreate() {
-    PresetPyramidCreator ppc = new PresetPyramidCreator(1, 1, 1, 0);
-    ArrayList<Preset> expected = new ArrayList<>();
-    //expected.add(new PresetFactory().createPreset(0, 180), )
+  public void testCreatePositions1x1x1()
+          throws CameraConnectionException {
+    Collection<ZoomPosition> actualPositons =  new PresetPyramidCreator(1, 1, 1, 0).generatePositions(cam);
+    Collection<ZoomPosition> expectedPositions = new ArrayList<>();
+    expectedPositions.add(new ZoomPosition(cam.getPosition(), cam.getZoomPosition()));
 
+    Assert.assertEquals(expectedPositions, actualPositons);
   }
 
   @Test
-  public final void testCreatePositionsOneOne()
-          throws InterruptedException, CameraConnectionException, TimeoutException {
-    PresetPyramidCreator ppc = new PresetPyramidCreator(1, 1, 1, 0);
-    ppc.createPresets(cam);
-    verify(cam).moveTo(eq(new Position(0, 180.0)), anyInt(), anyInt());
+  public void testCreatePositions2x1x1()
+          throws CameraConnectionException {
+    Collection<ZoomPosition> actualPositons =  new PresetPyramidCreator(2, 1, 1, 0).generatePositions(cam);
+    Collection<ZoomPosition> expectedPositions = new ArrayList<>();
+    expectedPositions.add(new ZoomPosition(0.0,
+                                           cam.getPosition().getTilt()-(IPCamera.VERTICAL_FOV_MAX/2),
+                                           cam.getZoomPosition()));
+    expectedPositions.add(new ZoomPosition(0.0,
+                                           cam.getPosition().getTilt()+(IPCamera.VERTICAL_FOV_MAX/2),
+                                           cam.getZoomPosition()));
+    Assert.assertEquals(expectedPositions, actualPositons);
   }
 
   @Test
-  public final void testCreatePositionsTwoByOne()
-          throws InterruptedException, CameraConnectionException, TimeoutException {
-    PresetPyramidCreator ppc = new PresetPyramidCreator(2, 1, 1, 0);
-    ppc.createPresets(cam);
-
-
-    verify(cam).moveTo(eq(new Position(0, 180-(IPCamera.HORIZONTAL_FOV_MAX/2))), anyInt(), anyInt());
-    verify(cam).moveTo(eq(new Position(0, 180+(IPCamera.HORIZONTAL_FOV_MAX/2))), anyInt(), anyInt());
+  public void testCreatePositions1x2x1()
+          throws CameraConnectionException {
+    Collection<ZoomPosition> actualPositons =  new PresetPyramidCreator(1, 2, 1, 0).generatePositions(cam);
+    Collection<ZoomPosition> expectedPositions = new ArrayList<>();
+    expectedPositions.add(new ZoomPosition(cam.getPosition().getPan()-(IPCamera.HORIZONTAL_FOV_MAX/2),
+            180.0,
+            cam.getZoomPosition()));
+    expectedPositions.add(new ZoomPosition(cam.getPosition().getPan()+(IPCamera.HORIZONTAL_FOV_MAX/2),
+            180.0,
+            cam.getZoomPosition()));
+    Assert.assertEquals(expectedPositions, actualPositons);
   }
 
+  @Override
+  public void testCreate() {
 
-  @Test
-  public final void testCreatePositionsOneByTwo()
-          throws InterruptedException, CameraConnectionException, TimeoutException {
-    PresetPyramidCreator ppc = new PresetPyramidCreator(1, 2, 1, 0);
-    ppc.createPresets(cam);
-
-    verify(cam).moveTo(eq(new Position(-60.3, 180.0)), anyInt(), anyInt());
-    verify(cam).moveTo(eq(new Position(60.3, 180.0)), anyInt(), anyInt());
   }
 
 }
