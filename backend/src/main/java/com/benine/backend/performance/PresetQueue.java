d3a2600e79d81141af55c90abd64707c367f231d
NaomidR <naomi-ah@hotmail.com>
2016-06-06 13:02:29 +0200

    Add first methods presetQueue

--- backend/src/main/java/com/benine/backend/performance/PresetQueue.java
+++ backend/src/main/java/com/benine/backend/performance/PresetQueue.java
@@ -1,20 +1,30 @@
 package com.benine.backend.performance;
 
+import com.benine.backend.preset.Preset;
+
+import java.util.LinkedHashMap;
+
+
+
 public class PresetQueue {
   
   private int lastValue = 0;
+  private LinkedHashMap<Integer,Preset> queue = new LinkedHashMap<Integer,Preset>();
+  
   /**
    * Returns the presetQueue.
+   * @return queue the queue that is used.
    */
-  private void getQueue(){
-    
+  private LinkedHashMap getQueue() {
+    return this.queue;
   }
   
   /**
    * Delete a preset at a given place.
    * @param place the place of the preset to be deleted.
    */
-  private void deletePreset(int place){
+  private void deletePreset(int place) {
+    queue.remove(place);
     lastValue--;
   }
   
@@ -22,15 +32,19 @@ public class PresetQueue {
   /**
    * Replace a preset on a certain place.
    * @param place the place of the preset to be updated.
+   * @param preset the preset that will replace the old preset on a certain place.
    */
-  private void update(int place) {
-    
+  private void update(int place, Preset preset) {
+    //put replaces the old value if there is already a mapping for this key.
+    queue.put(place, preset); 
   }
   
   /**
    * Add a preset at the end of the queue.
+   * @param preset the preset to be added at the end.
    */
-  private void addPresetEnd() {
+  private void addPresetEnd(Preset preset) {
+    queue.put(lastValue + 1, preset);
     lastValue++;
   }
   
@@ -38,8 +52,16 @@ public class PresetQueue {
    * Insert preset at given place.
    * @param place the place of the preset to be inserted.
    */
-  private void put(int place){
+  private void put(int place) {
     lastValue++;
   }
 
+  
+  /**
+   * Getter for the last key.
+   * @return the last value in the hashmap.
+   */
+  private int getLastValue() {
+    return lastValue;
+  }
 }
