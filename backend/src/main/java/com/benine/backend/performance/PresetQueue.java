d6ec2cf916384d779e19b7b8ecb36c01dcb06d49
NaomidR <naomi-ah@hotmail.com>
2016-06-07 18:47:30 +0200

    Change datastructure and implement methods.

--- backend/src/main/java/com/benine/backend/performance/PresetQueue.java
+++ backend/src/main/java/com/benine/backend/performance/PresetQueue.java
@@ -2,20 +2,25 @@ package com.benine.backend.performance;
 
 import com.benine.backend.preset.Preset;
 
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 
 
+
+/**
+ * Class that handles making a queue of presets.
+ * Created by Team BeNine.
+ */
 public class PresetQueue {
   
-  private int lastValue = 0;
-  private LinkedHashMap<Integer,Preset> queue = new LinkedHashMap<Integer,Preset>();
+  private int lastValue = -1;
+  private ArrayList<Preset> queue = new ArrayList<Preset>();
   
   /**
    * Returns the presetQueue.
    * @return queue the queue that is used.
    */
-  private LinkedHashMap getQueue() {
+  public ArrayList<Preset> getQueue() {
     return this.queue;
   }
   
@@ -23,45 +28,45 @@ public class PresetQueue {
    * Delete a preset at a given place.
    * @param place the place of the preset to be deleted.
    */
-  private void deletePreset(int place) {
+  public void deletePreset(int place) {
     queue.remove(place);
     lastValue--;
   }
-  
-  
+   
   /**
    * Replace a preset on a certain place.
    * @param place the place of the preset to be updated.
    * @param preset the preset that will replace the old preset on a certain place.
    */
-  private void update(int place, Preset preset) {
-    //put replaces the old value if there is already a mapping for this key.
-    queue.put(place, preset); 
+  public void update(int place, Preset preset) {
+    queue.set(place, preset); 
   }
   
   /**
    * Add a preset at the end of the queue.
    * @param preset the preset to be added at the end.
    */
-  private void addPresetEnd(Preset preset) {
-    queue.put(lastValue + 1, preset);
+  public void addPresetEnd(Preset preset) {
+    queue.add(preset);
     lastValue++;
   }
   
   /**
    * Insert preset at given place.
    * @param place the place of the preset to be inserted.
+   * @param preset the preset to be inserted in the queue. 
    */
-  private void put(int place) {
+  public void insertPreset(int place, Preset preset) {
+    queue.add(place, preset);
     lastValue++;
   }
 
   
   /**
-   * Getter for the last key.
-   * @return the last value in the hashmap.
+   * Getter for the size of the queue.
+   * @return the last value in the arraylist.
    */
-  private int getLastValue() {
+  public int getSize() {
     return lastValue;
   }
 }
