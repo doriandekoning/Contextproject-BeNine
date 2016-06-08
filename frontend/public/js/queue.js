94ac78b00a170ad0c11620c56f20e2d936b250dc
jochem725 <jochemlugtenburg@gmail.com>
2016-06-08 11:27:23 +0200

    Add Performance objects representing performances

---
+++ frontend/public/js/queue.js
@@ -0,0 +1,13 @@
+/**
+* Performance object containing all information about a performance Queue.
+*/
+function Performance(id, name, queue) {
+	this.id = id;
+	this.name = name;
+	this.queue = queue;
+}
+
+Performance.prototype = {
+
+};
+
