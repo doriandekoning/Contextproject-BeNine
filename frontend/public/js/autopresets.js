634aa782eb8337fb4e97a7a810f5631f77a74634
kingdorian <doriandekoning@gmail.com>
2016-06-09 21:07:18 +0200

    Fix lines not visible at right and bottom side preview autocreate

--- frontend/public/js/autopresets.js
+++ frontend/public/js/autopresets.js
@@ -1,23 +1,26 @@
 $( ".auto-presets-modal").on("shown.bs.modal", function(e) {
+  var offset = 1;
   var canvas = document.getElementById('previewCanvas')
   var context = canvas.getContext('2d');
   var imageObj = new Image();
   imageObj.onload = function () {
-    context.drawImage(imageObj, 0, 0);
+    context.drawImage(imageObj, offset, offset);
     context.strokeStyle = "#FF0000";
+    context.lineWidth=0.5;
     $.get("/api/backend/presets/autocreatesubviews?rows=2&levels=3&columns=2", function(data) {
        var subViews = JSON.parse(data);
        for ( var i = 0; i < subViews.SubViews.length; i++) {
          var height = (canvas.height/100) * (subViews.SubViews[i].topLeft.y  - subViews.SubViews[i].bottomRight.y);
          var width = (canvas.width/100) * (subViews.SubViews[i].bottomRight.x  - subViews.SubViews[i].topLeft.x);
-         var x = 1 + ((canvas.width/100) * (subViews.SubViews[i].topLeft.x));
-         var y = 1 + ((canvas.height/100) *  (100 -subViews.SubViews[i].topLeft.y));
+         var x = ((canvas.width/100) * (subViews.SubViews[i].topLeft.x));
+         var y = ((canvas.height/100) *  (100 -subViews.SubViews[i].topLeft.y));
          console.log(canvas.width);
          console.log("x:"+ x + ",y: " + y + ", width:" + width + ",height:"+ height);
          context.strokeRect(x, y, width, height)
        }
      });
   };
-
-  imageObj.src = '/api/backend/camera/1/mjpeg?width='+canvas.width + '&height=' + canvas.height;
+  var imageWidth = canvas.width - (offset * 2);
+  var imageHeight = canvas.height - (offset * 2);
+  imageObj.src = '/api/backend/camera/1/mjpeg?width='+ imageWidth + '&height=' + imageHeight;
 })
