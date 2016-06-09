ed0faf8d8dfeac4c51c379207a226858cf772527
jochem725 <jochemlugtenburg@gmail.com>
2016-06-09 15:05:03 +0200

    Add live view preset switching

--- frontend/public/js/liveview.js
+++ frontend/public/js/liveview.js
@@ -2,18 +2,16 @@
  * All methods related to the queue table.
  */
 var tablesize = 0;
+var position = -1;
+var currentperformance;
+var current;
+var next;
 
 function livetest() {
     performances = [];
     performances.push(new Performance(1, "Test Performance", [1, 2]));
 
     loadPerformanceDropdown()
-}
-
-/**
- * Loads the available performances from the backend.
- */
-function loadPerformances() {
 }
 
 function loadPerformanceDropdown() {
@@ -26,17 +24,32 @@ function loadPerformanceDropdown() {
         var li = $("<li></li>");
         var a = $("<a></a>");
         a.text(performance['name']);
+        a.data(performance);
+        a.click(loadPerformance);
 
         li.append(a);
         list.append(li);
     }
+}
 
+function loadPerformance() {
+    var performance = $(this).data();
+    currentperformance = performance;
+
+    var dropdown = $('#selectperformance-button');
+    dropdown.text(performance['name'] + ' ');
+    dropdown.append($("<span class='caret'></span>"));
+
+    var performance_presets = performance['presets'];
+
+    loadTable(performance_presets);
+    updateCurrentUpNext(position, position + 1, performance_presets[-1], performance_presets[0]);
 }
 
 /**
  * Loads the table.
  */
-function loadTable() {
+function loadTable(presets) {
     var table = $('#queue_table');
 
     table.children().remove();
@@ -54,8 +67,9 @@ function loadTable() {
 function setCurrent(preset) {
     var view = $('#select-current');
 
-    if (preset!== undefined) {
+    if (preset !== undefined) {
         setQueueView(view, preset);
+        preset.recallPreset();
     } else {
         setQueueViewPlaceholder(view, "holder.js/128x77?auto=yes&text=Current&bg=85ca85");
     }
@@ -68,22 +82,49 @@ function setCurrent(preset) {
 function setUpNext(preset) {
     var view = $('#select-upnext');
 
-    if (preset!== undefined) {
+    if (preset !== undefined) {
         setQueueView(view, preset);
     } else {
-        setQueueViewPlaceholder(view, "holder.js/128x77?auto=yes&text=Up Next&bg=f1bf7c");
+        setQueueViewPlaceholder(view, "holder.js/128x77?auto=yes&text=Up%20Next&bg=f1bf7c");
     }
 }
+
+function setRowUpNext(count) {
+    var table = $('#queue_table');
+
+    table.children().eq(0).children().removeClass('upnext');
+
+    if (count > -1) {
+        var row = table.children().eq(0).children().eq(count);
+        if (row !== undefined) {
+            row.addClass('upnext');
+        }
+    }
+}
+
+function setRowCurrent(count) {
+    var table = $('#queue_table');
+
+    table.children().eq(0).children().removeClass('current');
+
+    if (count > -1) {
+        var row = table.children().eq(0).children().eq(count);
+        if (row !== undefined) {
+            row.addClass('current');
+        }
+    }
+}
+
 
 function setQueueViewPlaceholder(view, image) {
     view.find('img').replaceWith($('<img data-src=' + image + ' class="col-xs-9">'));
     view.find('.preset_title').text("-");
     view.find('.preset_camera_number').text("-");
-    Holder.run({});
+    Holder.run();
 }
 
 function setQueueView(view, preset) {
-    view.find('img').attr("src", "/api/backend" + preset['image']);
+    view.find('img').replaceWith($("<img src='/api/backend" + preset['image'] + "' class='col-xs-9'>"));
     view.find('.preset_title').text("Preset " + preset['id']);
     view.find('.preset_camera_number').text(preset['cameraid']);
 }
@@ -102,4 +143,32 @@ function createRow(preset) {
     row.append("<td>" + preset['tags'] + "</td>");
 
     return row;
+}
+
+function nextPreset() {
+    var performance_presets = currentperformance['presets'];
+
+    if (position < (performance_presets.length - 1)) {
+        position = position + 1;
+        updateCurrentUpNext(position, position + 1, performance_presets[position], performance_presets[position + 1])
+    }
+}
+
+function prevPreset() {
+    var performance_presets = currentperformance['presets'];
+
+
+    if (position > -1) {
+        position = position - 1;
+        updateCurrentUpNext(position, position + 1, performance_presets[position], performance_presets[position + 1])
+    }
+}
+
+function updateCurrentUpNext(curpos, nextpos, current, upnext) {
+    setRowCurrent(curpos);
+
+    setCurrent(current);
+
+    setRowUpNext(nextpos);
+    setUpNext(upnext);
 }
