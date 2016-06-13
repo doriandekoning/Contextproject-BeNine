b45f09b32d3312a2c96a3f8fc7cf980d263bda7f
kingdorian <doriandekoning@gmail.com>
2016-06-13 17:29:23 +0200

    Replace sliders autopresetcreation with buttons

--- frontend/public/js/autopresets.js
+++ frontend/public/js/autopresets.js
@@ -1,4 +1,40 @@
+var columns = 3;
+var levels = 3;
+var rows = 3;
+
 $( ".auto-presets-modal").on("shown.bs.modal", function(e) {
+  showSubViews();
+})
+
+
+function autoCreatePresets() {
+  var name = $('#auto_preset_name').val();
+	var presetTag = $('#auto_preset_tags').val();
+  if (currentcamera !== undefined) {
+    $.get("/api/backend/presets/autocreatepresets?camera="+currentcamera+"&rows="+rows+"&levels="+levels+"&columns="+columns+"&name="+name + "&tags="+presetTag, function(data) {
+
+    });
+  }
+
+}
+
+function updateColumnSlider(newColumns) {
+  columns = newColumns;
+  showSubViews();
+}
+
+function updateRowSlider(newRows) {
+  rows = newRows;
+  showSubViews();
+}
+
+function updateLevelSlider(newLevels) {
+  levels = newLevels;
+  showSubViews();
+}
+
+
+function showSubViews() {
   var offset = 1;
   var canvas = document.getElementById('previewCanvas')
   var context = canvas.getContext('2d');
@@ -7,7 +43,7 @@ $( ".auto-presets-modal").on("shown.bs.modal", function(e) {
     context.drawImage(imageObj, offset, offset);
     context.strokeStyle = "#FF0000";
     context.lineWidth=0.5;
-    $.get("/api/backend/presets/autocreatesubviews?rows=2&levels=3&columns=2", function(data) {
+    $.get("/api/backend/presets/autocreatesubviews?rows="+rows+"&levels="+levels+"&columns="+columns, function(data) {
        var subViews = JSON.parse(data);
        for ( var i = 0; i < subViews.SubViews.length; i++) {
          var height = (canvas.height/100) * (subViews.SubViews[i].topLeft.y  - subViews.SubViews[i].bottomRight.y);
@@ -21,16 +57,4 @@ $( ".auto-presets-modal").on("shown.bs.modal", function(e) {
   var imageWidth = canvas.width - (offset * 2);
   var imageHeight = canvas.height - (offset * 2);
   imageObj.src = '/api/backend/camera/'+currentcamera+'/mjpeg?width='+ imageWidth + '&height=' + imageHeight;
-})
-
-
-function autoCreatePresets() {
-  var name = $('#auto_preset_name').val();
-	var presetTag = $('#auto_preset_tags').val();
-  if (currentcamera !== undefined) {
-    $.get("/api/backend/presets/autocreatepresets?camera="+currentcamera+"&rows=2&levels=3&columns=2&name="+name + "&tags="+presetTag, function(data) {
-
-    });
-  }
-
 }
