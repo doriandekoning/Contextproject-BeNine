3d4f1e4bf33f08493b6bfe1e001c7cd731cfa719
kingdorian <doriandekoning@gmail.com>
2016-06-16 10:30:03 +0200

    Remove creator after autopreset creation proces`

--- backend/src/main/java/com/benine/backend/http/AutoPresetCreationHandler.java
+++ backend/src/main/java/com/benine/backend/http/AutoPresetCreationHandler.java
@@ -59,6 +59,7 @@ public class AutoPresetCreationHandler extends AutoPresetHandler  {
               + camID, e);
       respondFailure(request, httpServletResponse);
     } finally {
+      creators.remove(cam.getId());
       request.setHandled(true);
     }
 
